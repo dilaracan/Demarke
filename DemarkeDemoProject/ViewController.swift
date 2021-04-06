a02e6dc7dafebc71f365a4a1b2b8eb5165dcd4f2
dilaracan <dilaracan17@gmail.com>
2021-04-06 12:49:03 +0300

    Initial Commit

---
+++ DemarkeDemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemarkeDemoProject
+//
+//  Created by Dilara Can on 6.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
